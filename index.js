308f99cb15eb492fe5ea685f657d7214b6054637
cawfeecake <48775802+cawfeecake@users.noreply.github.com>
2024-10-07 05:46:16 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+function getKey() {
+  const input = document.getElementById("key");
+  return input.value;
+}
+
 function getText() {
-  const textbox = document.querySelector("#message");
+  const textbox = document.getElementById("text");
   const text = textbox.value;
   const encoder = new TextEncoder();
   return encoder.encode(text);
 }
 
-function getTextEncrypt(key) {
-  const text = getText();
+function encrypt(text, key) {
   return window.crypto.subtle.encrypt({name: "RSA-OAEP"}, key, text);
+}
+
+function setEncrypted() {
+  const encryptedDisplay = document.getElementById("encrypted");
+  const key = getKey();
+  const text = getText();
+  encryptedDisplay.innerText = encrypt(text, key);
 }
