2ba079a27f859a4a7fedd89bfd01ab55a10d4a14
cawfeecake <48775802+cawfeecake@users.noreply.github.com>
2024-10-10 03:48:34 -0700

    Update `index.js` encryption functions

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ function getKey() {
   return input.value;
 }
 
+function setKey(key) {
+  document.getElementById("key").value = key;
+}
+
+async function generateKey() {
+  const keyProps = {name: "AES-CBC", length: 128};
+  const key = await window.crypto.subtle.generateKe(keyProps, true, ["encrypt", "decrypt"]);
+  const keyBuffer = await window.crypto.subtle.exportKey("raw", key)
+  setKey(new Uint8Array(keyBuffer))
+}
+
+function getSalt() {
+  const input = document.getElementById("salt");
+  return input.value;
+}
+
+function setSalt(salt) {
+  document.getElementById("salt").value = salt;
+}
+
+function generateSalt() {
+  const salt = window.crypto.getRandomValues(new Uint8Array(16));
+  setSalt(salt)
+}
+
 function getText() {
   const textbox = document.getElementById("text");
   const text = textbox.value;
@@ -10,13 +35,17 @@ function getText() {
   return encoder.encode(text);
 }
 
-function encrypt(text, key) {
-  return window.crypto.subtle.encrypt({name: "RSA-OAEP"}, key, text);
+function setEncrypted(encrypted) {
+  const encryptedDisplay = document.getElementById("encrypted");
+  encryptedDisplay.innerText = encrypted;
 }
 
-function setEncrypted() {
-  const encryptedDisplay = document.getElementById("encrypted");
+async function encrypt() {
+  const salt = getSalt();
+  const encryptProps = {name: "AES-CBC", iv: salt};
+  
   const key = getKey();
   const text = getText();
-  encryptedDisplay.innerText = encrypt(text, key);
+  const encrypted = await window.crypto.subtle.encrypt(encryptProps, key, text);
+  setEncrypted(encrypted)
 }
